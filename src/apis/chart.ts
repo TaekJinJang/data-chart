c718812a2e75b3294e4c4b9e04e089dc19c74cad
Taekjin Jang <xorwls1500@naver.com>
2023-09-10 17:39:51 +0900

    rename : apis/chart의 type을 type 폴더로 옮김

--- src/apis/chart.ts
+++ src/apis/chart.ts
@@ -1,20 +1,14 @@
+import {chartDataTypes} from 'types/chart';
 import {instance} from './instance';
 
 const DB_JSON_PATH = 'data/mock.json';
 
-export interface getChartResponse {
-    type: string;
-    version: number;
-    response: {
-        [key: string]: {id: string; value_area: number; value_bar: number};
-    };
-}
-
 export const getChartData = async () => {
     try {
-        const response = await instance.get<getChartResponse>(DB_JSON_PATH);
+        const response = await instance.get<chartDataTypes>(DB_JSON_PATH);
         return response.data;
     } catch (error) {
         console.error('fetchingError:', error);
+        throw error;
     }
 };
---
+++ src/types/chart.ts
@@ -0,0 +1,7 @@
+export interface chartDataTypes {
+    type: string;
+    version: number;
+    response: {
+        [key: string]: {id: string; value_area: number; value_bar: number};
+    };
+}
