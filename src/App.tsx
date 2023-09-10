eefe394d1d7bd8b153407c7f1f76d3e2a49457b2
Taekjin Jang <xorwls1500@naver.com>
2023-09-10 14:01:26 +0900

    setting: vite,컨벤션,절대경로,디렉토리 구조 설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Router} from 'Router';
+import {RouterProvider} from 'react-router-dom';
+
+function App() {
+    return (
+        <>
+            <RouterProvider router={Router} />
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+
+    return(
+        <div>gdgd</div>
+    )
+}
+
+export default Home
