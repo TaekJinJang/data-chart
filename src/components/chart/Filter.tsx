00313d6913f28c0777d82311303e0eb437e36b30
Taekjin Jang <xorwls1500@naver.com>
2023-09-11 14:36:04 +0900

    feat: 지역 필터링을 위한 버튼 추가

--- src/components/chart/Filter.tsx
+++ src/components/chart/Filter.tsx
@@ -1,3 +1,31 @@
-const Filter = () => {};
+import useChartData from 'hooks/useChartData';
+import styled from 'styled-components';
+
+const Filter = () => {
+    const {idList} = useChartData();
+    const filterList = [...new Set(idList)];
+    return (
+        <Container>
+            {filterList.map((filter, index) => {
+                return (
+                    <FilterButton id={filter} value={filter}>
+                        {filter}
+                    </FilterButton>
+                );
+            })}
+        </Container>
+    );
+};
 
 export default Filter;
+
+const Container = styled.div`
+    margin-left: 50px;
+`;
+
+const FilterButton = styled.button`
+    border-radius: 50px;
+    border-color: gray;
+    margin-left: 20px;
+    display: inline-block;
+`;
