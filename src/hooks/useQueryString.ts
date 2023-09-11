bb47c57ff90086818c598349617e16ddef9ba2ff
Taekjin Jang <xorwls1500@naver.com>
2023-09-11 14:47:29 +0900

    feat: 쿼리스트링 커스텀 훅 생성

---
+++ src/hooks/useQueryString.ts
@@ -0,0 +1,23 @@
+import {useSearchParams} from 'react-router-dom';
+
+export default function useQuerystring() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const queries = searchParams.getAll('filter');
+
+    const addQuery = (value: string) => {
+        if (value === undefined) return;
+        searchParams.append('filter', value);
+        setSearchParams(searchParams);
+    };
+
+    const deleteQuery = (value: string) => {
+        searchParams.delete('filter');
+        queries
+            .filter(query => query !== value)
+            .forEach(query => searchParams.append('filter', query));
+        setSearchParams(searchParams);
+    };
+
+    return {queries, addQuery, deleteQuery};
+}
