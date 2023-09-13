ac9a341832f782db84c6ff875719dae2c6486147
Taekjin Jang <xorwls1500@naver.com>
2023-09-13 12:28:17 +0900

    chore: 린트 설정 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,16 +13,14 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+    plugins: ['@typescript-eslint'],
     rules: {
         'no-var': 'error', // var 금지
         'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
         'no-console': ['error', {allow: ['warn', 'error', 'info']}], // console.log() 금지
         eqeqeq: 'error', // 일치 연산자 사용 필수
         'dot-notation': 'error', // 가능하다면 dot notation 사용
-        'no-unused-vars': 1, // 사용하지 않는 변수 경고 (에러 대신)
+        '@typescript-eslint/no-unused-vars': 1, // 사용하지 않는 변수 경고 (에러 대신)
         'func-style': ['error', 'expression'], // 함수 스타일 지정
-
-        'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     },
 };
