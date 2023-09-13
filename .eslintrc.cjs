f2666a684abad3b4ff5bdeb6de5648abb63f50b2
Taekjin Jang <xorwls1500@naver.com>
2023-09-13 11:56:51 +0900

    chore: eslintrc 파일 하나로 통합

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,13 @@
 module.exports = {
     root: true,
-    env: {browser: true, es2020: true},
+    env: {
+        browser: true,
+        node: true,
+        es2020: true,
+    },
     extends: [
+        'react-app',
+        'prettier',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
@@ -10,6 +16,14 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
+        'no-var': 'error', // var 금지
+        'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
+        'no-console': ['error', {allow: ['warn', 'error', 'info']}], // console.log() 금지
+        eqeqeq: 'error', // 일치 연산자 사용 필수
+        'dot-notation': 'error', // 가능하다면 dot notation 사용
+        'no-unused-vars': 1, // 사용하지 않는 변수 경고 (에러 대신)
+        'func-style': ['error', 'expression'], // 함수 스타일 지정
+
         'react-refresh/only-export-components': [
             'warn',
             {allowConstantExport: true},
